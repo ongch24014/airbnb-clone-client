03492129053562ff36abf125f4f7a60ecf7205ee
ongchunheng <ocheng.90@gmail.com>
2023-02-05 11:46:25 +0800

    added routes

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,10 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <form>
+        <input type="email" placeholder="your@email.com"></input>
+        <input type="password" placeholder="password"></input>
+      </form>
+    </div>
+  );
+}
