a9e21c035724a254c459a31e3441908aa17f22aa
ongchunheng <ocheng.90@gmail.com>
2023-02-18 18:58:15 +0800

    complete

--- src/pages/PlacesPage.jsx
+++ src/pages/PlacesPage.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, Navigate, useParams } from "react-router-dom";
 import AccountNav from "../AccountNav";
+import PlaceImg from "../PlaceImg";
 
 export default function PlacesPlace() {
   const [places, setPlaces] = useState([]);
@@ -43,16 +44,10 @@ export default function PlacesPlace() {
                 className="flex cursor-pointer gap-4 bg-gray-100 p-4 rounded-2xl "
                 key={place._id}
               >
-                <div className="flex w-32 h-32 bg-gray-300 grow shrink-0">
-                  {place.photos.length > 0 && (
-                    <img
-                      className="object-cover"
-                      src={"http://localhost:4000/uploads/" + place.photos[0]}
-                      alt=""
-                    />
-                  )}
+                <div className="flex w-32 h-32 bg-gray-300 shrink-0">
+                  <PlaceImg place={place} />
                 </div>
-                <div className="grow-0 shrink">
+                <div className="grow shrink">
                   <h2 className="text-xl">{place.title}</h2>
                   <p className="text-sm mt-2">{place.description}</p>
                 </div>
